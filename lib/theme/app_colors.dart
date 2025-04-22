d1291d6d5d8cac4126cc80c5ace8ade784ecd7de
Bruno Risso <brunorisso58@gmail.com>
2025-04-22 09:13:28 -0300

    Adicionando paleta de cores

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF005684);
+  static const Color secondary = Color(0xFFD6EAF8);
+  static const Color background = Color(0xFFF2F4F7);
+  static const Color accent = Color(0xFFF4B400);
+  static const Color error = Color(0xFFFF3131);
+  static const Color white = Color(0xFFFFFFFF);
+}
